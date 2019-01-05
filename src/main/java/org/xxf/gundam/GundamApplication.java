7029f586a169de117f0da8f8d82302e853a177fc
徐向峰 <whutxxf@163.com>
2019-01-05 17:52:06 +0800

    初始化项目结构

---
+++ src/main/java/org/xxf/gundam/GundamApplication.java
@@ -0,0 +1,14 @@
+package org.xxf.gundam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GundamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GundamApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/org/xxf/gundam/GundamApplicationTests.java
@@ -0,0 +1,17 @@
+package org.xxf.gundam;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GundamApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
